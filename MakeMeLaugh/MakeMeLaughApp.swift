8e10b9bbadaa219c8ac43c205da6887624948837
Greg Alton <gregaltonemail@gmail.com>
2024-06-06 08:00:32 -0500

    Initial Commit

---
+++ MakeMeLaugh/MakeMeLaughApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeMeLaughApp.swift
+//  MakeMeLaugh
+//
+//  Created by Greg Alton on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MakeMeLaughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
